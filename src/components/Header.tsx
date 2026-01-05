f8d4ea413ad5655d4c22d09297f144be18e7ea3e
Seifkall <saifkall5@gmail.com>
2026-01-05 05:15:21 +0300

    add link to user profile in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import Avatar from "boring-avatars";
 import UserContext from "../context/UserContext";
 import { useContext } from "react";
+import { Navigate, Link } from "react-router-dom";
 
 export default function Header() {
     const userContext = useContext(UserContext);
     const userClaims = userContext?.user;
 
+    if (!userClaims) {
+        return <Navigate to="/auth/login" />;
+    }
+
     return (
         <div className="flex items-center h-full w-full justify-between ">
             <img src="/logo-text.svg" alt="Sketchbook" className="h-7" />
             <div className="flex items-center gap-2 px-4">
-                <Avatar name={userClaims?.username} colors={userClaims?.avatarColors} variant={userClaims?.avatarVariant} size={40} />
+                <Link to={`/users/${userClaims.id}`}>
+                    <Avatar name={userClaims.Username} colors={userClaims.avatarColors} variant={userClaims.avatarVariant} size={40} />
+                </Link>
             </div>
         </div>
     )
